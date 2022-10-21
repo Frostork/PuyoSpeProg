771f2e8412d770e17863b943acd3d7afd985a431
Frostork <91312714+Frostork@users.noreply.github.com>
2022-10-21 11:27:13 +0200

    A

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -7,25 +7,41 @@ public class Grid : MonoBehaviour
 {
     [SerializeField] public int height;
     [SerializeField] public int width;
-
+    [SerializeField] public Vector2 offSet;
     [SerializeField] public List<GameObject> puyos;
-
+    private int[,] grid;
+    
     private void Start()
     {
+        grid = new int[height,width];
         GridSetup();
+        ShowGrid();
     }
 
     private void GridSetup()
     {
-        int[,] grid = new int[height, width];
-
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
-                Instantiate(puyos[0], new Vector3(y, x, 0), Quaternion.identity);
+                
+                Instantiate(puyos[0], new Vector3(x+offSet.x, y+offSet.y, 0), Quaternion.identity);
             }
         }
     }
 
+    private void ShowGrid()
+    {
+        string gridShow = "GRID : \n";
+        
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                gridShow += grid[y, x];
+            }
+            gridShow += "\n";
+        }
+        print(gridShow);
+    }
 }
--- Assets/Scripts/PuyoManager.cs
+++ Assets/Scripts/PuyoManager.cs
@@ -1,10 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PuyoManager : MonoBehaviour
 {
     [SerializeField] private List<Sprite> _states;
-    
+    [SerializeField] public float timeBeforeMove;
+
+    public List<Vector2> movements;
+
+    private void Update()
+    {
+        timeBeforeMove -= Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Moving(movements[1]);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            Moving(movements[2]);
+        }
+        if (timeBeforeMove <= 0)
+        {
+            Moving(movements[0]);
+        }
+    }
+
+    private void Moving(Vector3 movement)
+    {
+        transform.position += movement;
+    }
     
 }
